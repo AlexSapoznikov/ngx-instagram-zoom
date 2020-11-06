d9e9443621c1feefad59df45177a7bc068088652
Alex Sapoznikov <alex@nodeswat.com>
2020-11-06 15:11:17 +0200

    Import common module

--- projects/ngx-instagram-zoom/src/lib/ngx-instagram-zoom.module.ts
+++ projects/ngx-instagram-zoom/src/lib/ngx-instagram-zoom.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { NgxInstagramZoomComponent } from './ngx-instagram-zoom.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [NgxInstagramZoomComponent],
   imports: [
+    CommonModule
   ],
   exports: [NgxInstagramZoomComponent]
 })
